a7db545fae2ca7bd791cf649ba9cbe992a4dc54d
Sailesh25012001 <84961470+Sailesh25012001@users.noreply.github.com>
2022-09-15 20:24:27 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,81 @@
+// alert("Hello");
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+  console.log(userClickedPattern);
+});
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+  if(!started)
+  {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence(){
+  userClickedPattern=[];
+  level++;
+  $("h1").text("Level "+level);
+
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("."+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("."+currentColour).removeClass("pressed");
+  },100);
+}
+
+
+function checkAnswer(currentlevel){
+  if(userClickedPattern[currentlevel]===gamePattern[currentlevel])
+  {
+    console.log("success");
+    if(userClickedPattern.length===gamePattern.length)
+    {
+      setTimeout(function(){
+        nextSequence();
+      },1000);
+    }
+  }
+  else
+  {
+    console.log("wrong");
+    var as = new Audio("sounds/wrong.mp3");
+    as.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    },200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver(){
+  level=0;
+  gamePattern=[];
+  started=false;
+}
